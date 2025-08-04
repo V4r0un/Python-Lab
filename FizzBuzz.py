53ef918e1269f0e4504866b1eaaed3f49e18f613
V4r0un <varunkhandelwal0307@gmail.com>
2025-08-04 10:16:06 +0530

    Python 1

---
+++ FizzBuzz.py
@@ -0,0 +1,9 @@
+for i in range(1, 101):
+    if i % 3 == 0:
+        print ("Fizz Number")
+    elif i % 5 == 0:     
+        print ("Buzz Number")
+    elif i % 3 == 0 and i % 5 == 0:
+        print ("Fizz Buzz Number")
+    else:
+        print(i)
