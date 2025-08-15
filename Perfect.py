57c9befbd67ceed9588763435caeac5ec8dcad02
V4r0un <varunkhandelwal0307@gmail.com>
2025-08-15 09:30:36 +0530

    Python 22

---
+++ Perfect.py
@@ -0,0 +1,8 @@
+num = (int(input("Enter a Number = ")))
+sum = 0
+for i in range(1, num):
+    sum += i
+if sum == num:
+    print(f"{num} is a Perfect Number")
+else:
+    print(f"{num} is a Perfect Number")
